3cc726ecbf35c661b77e0da84be6a753863488fc
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-11-29 02:30:49 -0500

    benchmark test

---
+++ build/CMakeFiles/integrate.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/integrate.dir/integral.cpp.o"
+  "CMakeFiles/integrate.dir/integral.cpp.o.d"
+  "integrate"
+  "integrate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/integrate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
