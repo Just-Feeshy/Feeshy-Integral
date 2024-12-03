53a0ebfc5dcb6d8c4a6bf4d1a6458a5cf4af8382
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-12-03 15:10:31 -0500

    Yay

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+#include <program.h>
+
 int main(int argc, char** argv) {
+    program_init("Eath", 800, 600);
     return 0;
 }
--- src/program.c
+++ src/program.c
@@ -1,4 +1,5 @@
 #include <program.h>
+#include <stdio.h>
 
 void program_init(const char* name, int w, int h) {
     printf("Initializing program %s with dimensions %dx%d\n", name, w, h);
