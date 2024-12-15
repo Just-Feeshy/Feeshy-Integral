b5e1288411faeabaa36e521b7f59e44dbf5b1824
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2024-12-15 14:28:23 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <program.h>
 #include <shader.h>
+#include <pipeline.h>
+#include <screen.h>
 
 static shader_attribute* vert_attrs[] = {
     &(shader_attribute){"a_position", 0},
@@ -8,14 +10,22 @@ static shader_attribute* vert_attrs[] = {
 static shader_attribute* frag_attrs[] = {
 };
 
+static graphics_pipeline pipeline;
 static shader frag_shader;
 static shader vert_shader;
 
+const int width = 800;
+const int height = 600;
+
 int main(int argc, char** argv) {
-    program_init("Eath", 800, 600);
+    program_init("Eath", width, height);
 
     load_shader("shaders/vert.glsl", &vert_shader, SHADER_VERTEX, 1, vert_attrs);
     load_shader("shaders/frag.glsl", &frag_shader, SHADER_FRAGMENT, 0, frag_attrs);
+
+    pipeline_init(&pipeline);
+    pipeline_compile(2, &pipeline, (shader*[]){&vert_shader, &frag_shader});
+    screen_init(width, height);
 
     program_loop();
     program_destroy();
