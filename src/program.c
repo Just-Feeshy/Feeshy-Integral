6e1666cbede869eb6b94e4d089c491a4335cd32e
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2025-01-09 04:13:56 -0500

    Update program.c

--- src/program.c
+++ src/program.c
@@ -146,14 +146,14 @@ static void program_update() {
         NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
         NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
     {
-        static float inverse_fov = 45.0f;
+        static float fov = 45.0f;
 
         nk_layout_row_static(ctx, 20, 80, 1);
         nk_menu_item_label(ctx, "FOV", NK_TEXT_LEFT);
 
         nk_layout_row_static(ctx, 20, 100, 2);
-        if(nk_slider_float(ctx, 0, &inverse_fov, 80, 1)) {
-            world_update_fov(90.0 - inverse_fov);
+        if(nk_slider_float(ctx, 10, &fov, 90, 1)) {
+            world_update_fov(fov);
         }
 
         if (nk_button_label(ctx, "Reset Camera")) {
