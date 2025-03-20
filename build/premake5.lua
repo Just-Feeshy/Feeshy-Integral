56344b8eb87da0c9c835b1d391c537d6d455e706
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2025-03-20 10:08:18 -0400

    Add attribution for Premake

--- build/premake5.lua
+++ build/premake5.lua
@@ -1,3 +1,21 @@
+-- This project uses Premake, which is licensed under the BSD-3-Clause License.
+-- Copyright (C) 2002-2023 Jason Perkins and the Premake project.
+--
+-- Redistribution and use in source and binary forms, with or without modification,
+-- are permitted provided that the following conditions are met:
+--
+-- 1. Redistributions of source code must retain the above copyright notice,
+--    this list of conditions, and the following disclaimer.
+-- 2. Redistributions in binary form must reproduce the above copyright notice,
+--    this list of conditions, and the following disclaimer in the documentation
+--    and/or other materials provided with the distribution.
+-- 3. Neither the name of the copyright holders nor the names of its contributors
+--    may be used to endorse or promote products derived from this software
+--    without specific prior written permission.
+--
+-- Full license: https://github.com/premake/premake-core/blob/master/LICENSE.txt
+
+
 LANG = "C"
 SOLUTION_NAME = "program"
 SOLUTION_DIR = ".."
