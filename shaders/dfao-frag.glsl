b0f8a83a78bd8c68363612842066facf9259cdb9
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2025-05-23 03:59:12 -0400

    Remove bounding box

--- shaders/dfao-frag.glsl
+++ shaders/dfao-frag.glsl
@@ -210,7 +210,7 @@ vec4 render(vec2 uv) {
             vec3 p = ray_origin + t * ray_direction;
             vec3 normal = sdf_normal(p);
             float ao = computeDFAO(p, normal);
-            // color = texture(u_texture, v_position / u_resolution).rgb * ao;
+            color = texture(u_texture, v_position / u_resolution).rgb * ao;
         }
     }
 
--- src/gl_dfao.c
+++ src/gl_dfao.c
@@ -35,19 +35,6 @@ static void dfao_make_textures(geometry_pass* g_pass, Mesh mesh, AABB aabb) {
     }
 }
 
-static void init_bounding_box(Mesh mesh, AABB aabb) {
-    red_texture = texture_red_init();
-    mesh_b = gen_mesh_cube(aabb);
-    material_b = load_material_default();
-    material_b.maps[MATERIAL_MAP_ALBEDO].texture = &red_texture;
-}
-
-static void draw_bounding_box() {
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    draw_mesh(mesh_b, material_b);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-}
-
 void dfao_test_world(geometry_pass* g_pass) {
     shader frag_shader;
     shader vert_shader;
@@ -66,14 +53,12 @@ void dfao_test_world(geometry_pass* g_pass) {
     load_shader("shaders/baseworld-frag.glsl", &frag_shader, SHADER_FRAGMENT, 0, frag_attrs);
 
     model = load_model("assets/DamagedHelmet.glb");
-    // glm_translate_make(model.transform, (vec3){0.0f, -2.5f, -5.0f});
 
     Mesh mesh = model.meshes[index_mesh];
     g_pass->aabb = get_mesh_AABB(mesh);
     glm_vec3_add(g_pass->aabb.min, (vec3){-0.5f, -0.5f, -0.5f}, g_pass->aabb.min);
     glm_vec3_add(g_pass->aabb.max, (vec3){0.5f, 0.5f, 0.5f}, g_pass->aabb.max);
     dfao_make_textures(g_pass, mesh, g_pass->aabb);
-    // init_bounding_box(mesh, g_pass->aabb);
 
     pipeline_compile(2, g_pass->pipeline, (shader*[]){&vert_shader, &frag_shader});
     create_constant_location(g_pass->pipeline, "u_model");
@@ -83,7 +68,5 @@ void dfao_test_world(geometry_pass* g_pass) {
 void dfao_test_world_render() {
     set_uniform_int("u_texture", 0);
     set_uniform_mat4("u_model", model.transform);
-    draw_mesh(model.meshes[index_mesh], model.materials[model.mesh_material[index_mesh]]);
-    // draw_bounding_box();
-
+    draw_model(model);
 }
