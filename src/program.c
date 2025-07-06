24bfbdb887bb3aef698d837db702e724d2e3f7c2
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2025-07-06 18:04:59 -0400

    Fix

--- src/program.c
+++ src/program.c
@@ -209,10 +209,6 @@ void program_init(const char* name, int w, int h) {
         return;
     }
 
-    #ifdef NEED_EXT
-    opengl_init_ext();
-    #endif
-
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
 		printf("SDL_Init failed: %s\n", SDL_GetError());
 		return;
